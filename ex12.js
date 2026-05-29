20cf7c2c6fd1f9a875dfc4741944576f596509f4
Sarai <sarai@8e:07:aa:d2:d8:70.home>
2026-05-29 15:27:15 -0400

    Exercise 12 last one completed

--- ex12.js
+++ ex12.js
@@ -1,6 +1,9 @@
-s// Exercise 12: Write a function `calculateBMI` that takes two arguments: weight (in kg) and height (in meters).
+// Exercise 12: Write a function `calculateBMI` that takes two arguments: weight (in kg) and height (in meters).
 // The function should return the BMI (Body Mass Index), rounded to two decimal places.
 // The formula for BMI is: BMI = weight (kg) / height (m)^2. To get the squared value of height, just multiply height by itself.
 // Example: calculateBMI(70, 1.75) should return 22.86.
-
+  function calculateBMI(weight, height) {
+    let bmi = weight / (height * height);
+    return parseFloat(bmi.toFixed(2));
+  }
 console.log(calculateBMI(70, 1.75)); // Expected output: 22.86
