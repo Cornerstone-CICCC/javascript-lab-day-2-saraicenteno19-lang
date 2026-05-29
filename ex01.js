d43f9219609e943517f577f7c038d494ec4336d0
Sarai <sarai@8e:07:aa:d2:d8:70.home>
2026-05-29 12:12:19 -0400

    Exercise 01 completed

--- ex01.js
+++ ex01.js
@@ -1,5 +1,11 @@
 // Exercise 1: Write a function `capitalizeWords` that takes a string and returns the string
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
+ function capitalizeWords(str) {
+    return str
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 
+ }
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
