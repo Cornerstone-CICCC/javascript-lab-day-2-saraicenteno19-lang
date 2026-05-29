d81e5553a7a68f6ac571af670a21e93420e83b7f
Sarai <sarai@8e:07:aa:d2:d8:70.home>
2026-05-29 12:40:13 -0400

    Exercise 03 completed

--- ex03.js
+++ ex03.js
@@ -2,4 +2,17 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+  function findMissingNumber(arr) {
+    let numbers = [];
+    for (let i = 1; i <= arr.length + 1; i++) {
+        numbers.push(i);
+
+    }
+    for (let num of numbers) {
+        if (!arr.includes(num)) {
+            return num;
+        }
+    }
+
+  }
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
