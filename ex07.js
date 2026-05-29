6a587fe64f49dbed4e8fdbf28ae2ff6cbeb4757e
Sarai <sarai@8e:07:aa:d2:d8:70.home>
2026-05-29 14:00:41 -0400

    Exercise 07 completed

--- ex07.js
+++ ex07.js
@@ -2,6 +2,8 @@
 // and returns true if the email is valid, and false otherwise. Use basic validation
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
-
+    function validateEmail(email) {
+        return email. includes("@") && email.includes(".");
+    }
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
